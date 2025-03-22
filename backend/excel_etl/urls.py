21950f4e91e156561c431aff374d2cead415720b
SOMU <somuattaluri@gmail.com>
2025-03-22 16:14:51 -0700

    removing static files collection

--- backend/excel_etl/urls.py
+++ backend/excel_etl/urls.py
@@ -16,14 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http  import HttpResponse
-def health_check(request):
-    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('health/', health_check),
 ]
-
 
